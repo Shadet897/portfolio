fd3a40f0911ae98a64aa4791b8b24652fb2cd671
Shadet <kornelrybacki897@gmail.com>
2023-10-09 09:03:19 +0200

    changed color palette and overall design

--- forIndex.js
+++ forIndex.js
@@ -1,20 +1,17 @@
-function changeBackgroundColorToWhite() {
-    document.body.style.backgroundColor = "#FAF9F6";
+function changeBackgroundColorToGreen() {
+    document.body.style.backgroundColor = "#5CDB95";
     let elements = document.getElementsByClassName("overviewStyle");
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
         element.style.color = "black";
     }
-    document.getElementById("colorListHeading").style.color = "#000000";
 }
 
-function changeBackgroundColorToBlack() {
-    document.body.style.backgroundColor = "#000000";
+function changeBackgroundColorToBlue() {
+    document.body.style.backgroundColor = "#05386B";
     let elements = document.getElementsByClassName("overviewStyle");
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i];
         element.style.color = "white";
     }
-    document.getElementById("colorListHeading").style.color = "#ffffff";
-
 }
